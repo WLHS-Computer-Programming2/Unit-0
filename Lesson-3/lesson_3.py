3acc56748182c27ddf25827cb49d6a7ef8e57e98
Brandon Smith <smithb@wlwv.k12.or.us>
2025-02-09 22:38:45 -0800

    tic-tac-toe added

---
+++ Lesson-3/lesson_3.py
@@ -0,0 +1,13 @@
+'''
+Name: Mr. Smith
+Date: 2-9-25
+Unit: 1
+Topic: List Comprehensions and Dictionaries
+'''
+
+# List comprehensions
+
+
+# List comprehensions with conditionals
+
+# Dictionaries
